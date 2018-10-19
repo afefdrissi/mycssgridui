b14b7930a45dfa9aa494c8fb524a5be355d5dd2a
Afef <drissiafef1@gmail.com>
2018-10-19 01:46:37 +0100

    day 8

--- js.js
+++ js.js
@@ -2,5 +2,5 @@ $("aside>ul>li").click(function(){
     $(this).toggleClass("active");
 });
 for (var i=0;i<20;i++){
-    $(".list>ul").append('<li><input type="checkbox" name="" id=""><span> Product</span><span>Qty</span><span>Unit</span><span>Price</span><span>total</span><span>*</span></li>');
+    $(".list>ul").append('<li><input type="checkbox" name="" id=""><span> Product</span><span>Qty</span><span>Unit</span><span>Price</span><span>total</span><span class="delete"></span></li>');
 }
